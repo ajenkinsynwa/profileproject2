ac2b81e062e79feb4da80e63eedd6fbe1f7c5227
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2023-01-03 13:44:11 -0500

    updated nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
+import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './commponets/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       
     </div>
   );
