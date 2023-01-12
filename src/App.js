48fa81575fb1694564b327a40385a457ece63fd3
Alex Jenkins <108849441+ajenkinsynwa@users.noreply.github.com>
2023-01-11 21:32:09 -0500

    updeated newsletter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { NavBar } from './commponets/NavBar';
 import { Banner } from './commponets/Banner';
 import { Skills } from './commponets/Skills';
+import { Projects } from './commponets/Projects';
+import { Contact } from './commponets/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
